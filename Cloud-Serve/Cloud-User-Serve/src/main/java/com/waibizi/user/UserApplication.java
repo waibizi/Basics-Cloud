1258fa5a20bfe0bb554ff5c38e9243cc8f97d0f7
歪鼻子 <17876089816@163.com>
2021-01-30 23:09:51 +0800

    初始化工程，初始化注册服务发现demo

---
+++ Cloud-Serve/Cloud-User-Serve/src/main/java/com/waibizi/user/UserApplication.java
@@ -0,0 +1,19 @@
+package com.waibizi.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: 歪鼻子
+ * @CreateDate: 2021/1/30
+ * @Description: 用户管理启动器
+ * @Version: 1.0.0
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# 基于SpringCloud写的一个demo快速入门教程
+
+# Cloud技术选型
+注册中心
